c10bbac538789444a20dfb74926b6f483eb801bc
emilyallen3 <emily.allen8715@gmail.com>
2022-05-06 01:29:41 -0600

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -2,15 +2,18 @@
 #Emily Allen
 
 class Game:
+    #Created the parent class Game and 4 attributes
     name = "None Provided"
     publish_date = "01/01/1973"
     number_players = 1
     avg_duration = "0 hrs"
 
 class Video_Game(Game):
+    #Created a child class of Game with 2 attributes
     gaming_system = "Atari"
     online_play = True
 
 class Board_Game(Game):
+    #Created a child class of Game with 2 attributes
     number_dice = 0
     board_dimensions = "12 x 12 in"
