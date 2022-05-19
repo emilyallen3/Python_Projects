943c526a81e1f7b9ceecb6a4470236054d7d28a2
emilyallen3 <emily.allen8715@gmail.com>
2022-05-19 17:25:51 -0600

    Removed

--- Django _Hello_World/src/Hello_World/Hello_World/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def home(request):
-    names = {'Nathan', 'Emily', 'Daniel', 'Eleanor', 'Audrey'}
-    context = {
-        'names': names,
-    }
-    return render(request, "home.html", context)
