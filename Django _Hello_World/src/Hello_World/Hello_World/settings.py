1c4d420e35c2f080311d1679e0c42638d8a3cc21
emilyallen3 <97501659+emilyallen3@users.noreply.github.com>
2022-05-18 18:48:22 -0600

    Update settings.py

--- Django _Hello_World/src/Hello_World/Hello_World/settings.py
+++ Django _Hello_World/src/Hello_World/Hello_World/settings.py
@@ -117,10 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'resources'),]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
