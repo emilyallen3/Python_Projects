f5000bc621026f52aa3dcfe1914210389a3af7e1
emilyallen3 <97501659+emilyallen3@users.noreply.github.com>
2022-05-18 18:47:40 -0600

    Update settings.py

--- Django _Hello_World/src/Hello_World/Hello_World/settings.py
+++ Django _Hello_World/src/Hello_World/Hello_World/settings.py
@@ -21,7 +21,7 @@ BASE_DIR =Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!4^4vbjw$1+o!drpg-4&xq38dzuu^$qoq1!%lnn6@34pnm9w0!'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
